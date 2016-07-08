69948d23ffe246e400bf4deef2bd383807dfbf3c
fredkingham <fredkingham@gmail.com>
2016-07-08 10:01:51 +0100

    remove commented out code

--- pathway/static/js/pathway/controllers/multi_stage_default.js
+++ pathway/static/js/pathway/controllers/multi_stage_default.js
@@ -1,17 +1,2 @@
 angular.module('opal.controllers').controller("MultistageDefault", function(){
-    // this.valid = function(){
-    //     return true;
-    // };
-    //
-    // this.toSave = function(editing){
-    //     // does nothing;
-    // };
-    //
-    // this.showNext = function(editing){
-    //     return true;
-    // }
-    //
-    // scope.showNext = function(editing){
-    //     return true;
-    // }
 });
