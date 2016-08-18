ce5fc3cc09b0bc1fca810230a8c3b44f9cfecb03
fredkingham <fredkingham@gmail.com>
2016-08-18 16:35:01 +0100

    add a multiple save wrapper directive

--- pathway/static/js/pathway/directives.js
+++ pathway/static/js/pathway/directives.js
@@ -1,3 +1,88 @@
+directives.directive("saveMultipleWrapper", function($parse, Referencedata){
+  /*
+    a utility directive that allows us to code
+    as if we had an array of editings and save
+    them in a pathway appropriately
+    it adds into the child scope
+
+    models.subrecords an array of the appropriate models
+    remove a function that takes in an index and will remove that from the array
+    addAnother a function that will add another to an array
+
+    by default it will add an empty model if there isn't one already
+
+    example usage
+
+    <div save-multiple-wrapper="editing.treatment">
+      <div ng-repeat="editing in model.subrecords">
+        {% input field="Treatment.drug" %}
+        <button ng-click="remove($index)"></button>
+      </div>
+
+      <button ng-click="addAnother()"></button>
+    </div>
+  */
+  return {
+    scope: {
+      parentModel: "=saveMultipleWrapper",
+      initialiseEmpty: "=?initialiseEmpty",
+    },
+    transclude: true,
+    template: '<div ng-transclude></div>',
+    link: function(scope, element, attrs, ctrl, transclude){
+      var editingString = attrs.saveMultipleWrapper;
+
+      if(!scope.model_name){
+        scope.model_name = editingString.substr(editingString.indexOf(".")+1);
+      }
+
+      if(_.isUndefined(scope.initialiseEmpty)){
+        scope.initialiseEmpty = true;
+      }
+      var getModel = $parse(scope.model_name);
+
+      // in pathways we save multiple models of the same type as arrays
+      if(!_.isArray(scope.parentModel)){
+          scope.parentModel = [scope.parentModel];
+      }
+
+      if(!scope.parentModel.length && scope.initialiseEmpty){
+          scope.parentModel.push({});
+      }
+
+      // shallow copy not deep copy as angular copy can't
+      // deal with moments
+      scope.model = {'subrecords': _.map(scope.parentModel, function(row){
+          var result = {};
+          result[scope.model_name] = row;
+          return result;
+      })};
+
+      scope.remove = function($index){
+          scope.model.subrecords.splice($index, 1);
+      };
+
+      scope.addAnother = function(){
+          scope.model.subrecords.push({});
+      };
+
+      // deep watch any changes and when they're done
+      // copy them onto the parent model
+      scope.$watch("model.subrecords", function(){
+          var children = _.map(scope.model.subrecords, function(someEditing){
+              return getModel(someEditing);
+          });
+          scope.parentModel = children;
+      }, true);
+
+      transclude(scope, function(clone, scope) {
+          element.empty();
+          element.append(clone);
+      });
+    }
+  };
+});
+
 directives.directive("saveMultiple", function($parse, $rootScope, Referencedata){
   return {
     scope: {
@@ -56,24 +141,24 @@ directives.directive("saveMultiple", function($parse, $rootScope, Referencedata)
 
       // shallow copy not deep copy as angular copy can't
       // deal with moments
-      scope.model = {'allModels': _.map(scope.parentModel, function(row){
+      scope.model = {'subrecords': _.map(scope.parentModel, function(row){
           var result = {};
           result[scope.model_name] = row;
           return result;
       })};
 
       scope.remove = function($index){
-          scope.model.allModels.splice($index, 1);
+          scope.model.subrecords.splice($index, 1);
       };
 
       scope.addAnother = function(){
-          scope.model.allModels.push({});
+          scope.model.subrecords.push({});
       };
 
       // deep watch any changes and when they're done
       // copy them onto the parent model
-      scope.$watch("model.allModels", function(){
-          var children = _.map(scope.model.allModels, function(someEditing){
+      scope.$watch("model.subrecords", function(){
+          var children = _.map(scope.model.subrecords, function(someEditing){
               return getModel(someEditing);
           });
           scope.parentModel = children;
