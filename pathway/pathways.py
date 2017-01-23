2b732184d2204688e4456d6c8dfbd4337347879a
fredkingham <fredkingham@gmail.com>
2017-01-23 07:04:54 +0000

    a single step modal that allows one to just enter a single step and for that to be rendered nicely

--- pathway/pathways.py
+++ pathway/pathways.py
@@ -315,4 +315,10 @@ class ModalWizardPathway(Pathway, AbstractBase):
 
 class ModalPagePathway(Pathway, AbstractBase):
     template_url = "/templates/pathway/modal_page_pathway.html"
+    step_wrapper_template_url = "/templates/pathway/step_wrappers/page.html"
+    pathway_insert = ".modal-content"
+
+
+class SingleStepModalPathway(Pathway, AbstractBase):
+    template_url = "/templates/pathway/modal_page_pathway.html"
     pathway_insert = ".modal-content"
