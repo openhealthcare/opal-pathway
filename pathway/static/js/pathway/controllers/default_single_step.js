3e63fb016e037ba61e90c5e6f7f997cfe96ce881
fredkingham <fredkingham@gmail.com>
2017-03-27 10:15:19 +0100

    add a unit test for the default single step

--- pathway/static/js/pathway/controllers/default_single_step.js
+++ pathway/static/js/pathway/controllers/default_single_step.js
@@ -1,13 +1,7 @@
 angular.module('opal.controllers').controller(
   "DefaultSingleStep", function(scope, step, episode){
     if(scope.editing[step.model_api_name]){
-      // TODO change it so that its always an array
-      if(_.isArray(scope.editing[step.model_api_name]) && scope.editing[step.model_api_name].length){
-        scope.editing[step.model_api_name] = _.last(scope.editing[step.model_api_name]);
-      }
-      else{
-        scope.editing[step.model_api_name] = scope.editing[step.model_api_name];
-      }
+      scope.editing[step.model_api_name] = _.last(scope.editing[step.model_api_name]);
     }
     else if(episode){
       scope.editing[step.model_api_name] = episode.newItem(step.model_api_name);
