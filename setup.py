e53b0093ff32d2faf12af6e0d9026c4de08f87b2
fredkingham <fredkingham@gmail.com>
2017-06-05 10:09:24 +0100

    bumping the version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='opal-pathway',
-    version='0.2',
+    version='0.4',
     packages=find_packages(),
     include_package_data=True,
     license='LICENSE',
