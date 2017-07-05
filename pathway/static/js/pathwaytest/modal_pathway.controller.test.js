b0cdf8612b13612deb05c5a3cf494c126a23f447
fredkingham <fredkingham@gmail.com>
2017-07-05 10:44:49 +0100

    add in mkcontroller

--- pathway/static/js/pathwaytest/modal_pathway.controller.test.js
+++ pathway/static/js/pathwaytest/modal_pathway.controller.test.js
@@ -99,6 +99,7 @@ describe('ModalPathwayCtrl', function() {
   });
 
   it('should close the instance if the call back returns a string', function(){
+    mkController();
     $scope.pathway.pathwayResult.resolve();
     $scope.$apply();
     expect($modalInstance.close).toHaveBeenCalledWith();
